333574e25e3f2108b04a86875e847bbd786cd106
Lara Filippone <lara.filippone@gmail.com>
2022-05-03 22:48:39 +0200

    snack2 add variables with get

--- index.php
+++ index.php
@@ -53,7 +53,10 @@ Snack 2
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 */
 
-
+//passo tramite GET, nome mail e età
+$name = $_GET['name'];
+$email = $_GET['email'];
+$age = $_GET['age'];
 
 
 
